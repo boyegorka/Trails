74e0501aef614316bf7b420e1c075008ab23ee57
Egor Svistushkin <boyegorka@gmail.com>
2024-03-02 19:00:28 +0300

    POST request added for application

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const routesLink = "http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes?api_key=38d499da-b44a-4b4e-a3e7-b910ab1d2c93"
-// const guideLink = "http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/guides/23?api_key=38d499da-b44a-4b4e-a3e7-b910ab1d2c93"
-
+const mainLink = 'http://exam-2023-1-api.std-900.ist.mospolytech.ru/api'
+const apiKey = "?api_key=38d499da-b44a-4b4e-a3e7-b910ab1d2c93"
+const apiRoutes = {
+    guides: '/guides',
+    routes: '/routes',
+    orders: '/orders',
+};
 
 // RoutesTable consts
 const routesTable = document.getElementById("routes-table");
@@ -33,13 +37,40 @@ var filteredGuides = [];
 var languages = [];
 
 // Application registration
-const myModal = new bootstrap.Modal(document.getElementById('myModal'), {keyboard:false})
+const orderForm = document.getElementById("order-form");
+const routeNameModalElement = document.getElementById("route-name")
+const guideNameModalElement = document.getElementById("guide-name")
+const dateModalElement = document.getElementById("tour-date")
+const timeModalElement = document.getElementById("tour-time")
+const durationModalElement = document.getElementById("tour-duration")
+const personsModalElement = document.getElementById("people-count")
+const studentDiscountModalElement = document.getElementById("scolar-discount")
+const souvenirModalElement = document.getElementById("themed-souvenirs")
+const guidePriceModalElement = document.getElementById("tour-price")
+var modalGuide
+var modalRoute
+var applicationId = 0
+
+// Alert
+const alertPlaceholder = document.getElementById('alertPlaceholder')
+const alertType = {
+    primary: 'primary',
+    secondary: 'secondary',
+    success: 'success',
+    danger: 'danger',
+    warning: 'warning',
+    info: 'info',
+    light: 'light',
+    dark: 'dark',
+};
+
 
 // RoutesTable funcs
 function loadRoutes() {
+    const link = mainLink + apiRoutes.routes + apiKey
     let xhr = new XMLHttpRequest();
     xhr.responseType = "json";
-    xhr.open('GET', routesLink);
+    xhr.open('GET', link);
     xhr.send();
 
     xhr.onload = function () {
@@ -152,8 +183,8 @@ function setObjectsToRoutesSelect() {
 
 function showGuidesDiv(routeName, routeId) {
     currentPageGuidesTable = 1
-    document.getElementById(`routes-btn-${routeId}`).blur();
-    document.getElementById(`routes-btn-${routeId}`).classList.add('active');
+    // document.getElementById(`routes-btn-${routeId}`).blur();
+    // document.getElementById(`routes-btn-${routeId}`).classList.add('active');
     loadGuidesForRoute(routeId);
     guidesDiv.classList = 'class="container-fluid py-5';
     guidesDivTitle.innerHTML = `Доступные гиды по маршруту: ${routeName}`;
@@ -162,17 +193,17 @@ function showGuidesDiv(routeName, routeId) {
 
 // GuidesTable funcs
 function loadGuidesForRoute(routeId) {
-    const guidesLink = `http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes/${routeId}/guides?api_key=38d499da-b44a-4b4e-a3e7-b910ab1d2c93`;
+    const link = mainLink + apiRoutes.routes + `/${routeId}` + apiRoutes.guides + apiKey
 
     let xhr = new XMLHttpRequest();
     xhr.responseType = "json";
-    xhr.open('GET', guidesLink);
+    xhr.open('GET', link);
     xhr.send();
 
     xhr.onload = function () {
         let response = xhr.response;
         guides = response
-        console.log(guides)
+        // console.log(guides)
         getLanguages()
         filterGuides()
     };
@@ -216,7 +247,7 @@ function updateGuidesTable() {
     displayedData.forEach(item => {
         const row = document.createElement('tr');
         const avatarPath = loadAvatars(item)
-        row.innerHTML = `<td class="w-5 h-25 align-middle"><img src="${avatarPath}" class="img-fluid rounded" style="max-width: 50px; height: auto; alt="Photo"></td><td class="align-middle">${item["name"]}</td><td class="align-middle">${item["language"]}</td><td class="align-middle">${item["workExperience"]}</td><td class="align-middle">${item["pricePerHour"]+"₽"}</td><td class="align-middle text-end"><button id="btn" type="button" class="btn btn-outline-primary" onclick="registerApplication()">Оформить заявку</button></td>`;
+        row.innerHTML = `<td class="w-5 h-25 align-middle"><img src="${avatarPath}" class="img-fluid rounded" style="max-width: 50px; height: auto; alt="Photo"></td><td class="align-middle">${item["name"]}</td><td class="align-middle">${item["language"]}</td><td class="align-middle">${item["workExperience"]}</td><td class="align-middle">${item["pricePerHour"]+"₽"}</td><td class="align-middle text-end"><button id="btn" type="button" class="btn btn-outline-primary" onclick=loadRouteInfo(${item["id"]},${item["route_id"]})>Оформить заявку</button></td>`;
         guidesTableTBody.appendChild(row);
     });
 
@@ -292,8 +323,179 @@ function setLanguagesToGuidesSelect() {
     }
 }
 
-function registerApplication() {
+// Modal funcs
+function loadRoute(routeId) {
+    const link = mainLink + apiRoutes.routes + `/${routeId}` + apiKey
+
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+    xhr.open('GET', link);
+    xhr.send();
+
+    xhr.onload = function () {
+        modalRoute = xhr.response;
+        console.log(modalRoute["name"])
+        setModalRoute()
+    };
+
+    xhr.onerror = function() {
+        console.log('Ошибка');
+    };
+}
+
+function loadGuide(guideId) {
+    const link = mainLink + apiRoutes.guides + `/${guideId}` + apiKey
+
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+    xhr.open('GET', link);
+    xhr.send();
+
+    xhr.onload = function () {
+        modalGuide = xhr.response;
+        console.log(modalGuide)
+        setModalGuide()
+    };
+
+    xhr.onerror = function() {
+        console.log('Ошибка');
+    };
+}
+
+function setModalRoute() {
+    routeNameModalElement.value = modalRoute["name"]
+}
+
+function setModalGuide() {
+    guideNameModalElement.value = modalGuide["name"]
+    setPrice()
+}
+
+function loadRouteInfo(guideId, routeId) {
+    loadRoute(routeId)
+    loadGuide(guideId)
+    showRegistration()
+}
+
+function showRegistration() {
+    const myModal = new bootstrap.Modal(document.getElementById('myModal'))
     myModal.show()
+}
+
+function setPrice() {
+    var price = modalGuide["pricePerHour"]
+    var peopleCount = personsModalElement.value
+    var duration = durationModalElement.value
+    var studentDiscount = studentDiscountModalElement.checked
+    var themedSouvenirs = souvenirModalElement.checked
+
+    console.log(studentDiscount, themedSouvenirs)
+
+    price = price * peopleCount * duration
+
+    if (themedSouvenirs === true) {
+        price = price + (500 * peopleCount)
+    }
+
+    if (studentDiscount === true) {
+        price = price - (price * 15 / 100)
+    }
+
+    guidePriceModalElement.value = price + `₽`
+}
+
+function processFormData() {
+    var guideId = modalGuide["id"]
+    var routeId = modalRoute["id"]
+    var date = dateModalElement.value
+    var time = timeModalElement.value
+    var peopleCount = personsModalElement.value
+    var duration = durationModalElement.value
+    var studentDiscount = studentDiscountModalElement.checked
+    var themedSouvenirs = souvenirModalElement.checked
+    var price = guidePriceModalElement.value.slice(0, -1);
+
+    if (studentDiscount === true) {
+        studentDiscount = 1
+    } else {
+        studentDiscount = 0
+    }
+
+    if (themedSouvenirs === true) {
+        themedSouvenirs = 1
+    } else {
+        themedSouvenirs = 0
+    }
+
+    date = date
+    time = time + ':00'
+    price = parseInt(price)
+    peopleCount = parseInt(peopleCount)
+    duration = parseInt(duration)
+
+    applicationId += 1
+    
+    var params = {
+        guide_id: guideId,
+        route_id: routeId,
+        date: date,
+        time: time,
+        duration: duration,
+        persons: peopleCount,
+        price: price,
+        optionFirst: studentDiscount,
+        optionSecond: themedSouvenirs,
+        student_id: 21,
+        id: applicationId,
+    };
+    
+    postGuide(params)
+}
+
+function postGuide(data) {
+    const link = mainLink + apiRoutes.orders + apiKey;
+
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+    xhr.open('POST', link);
+
+    const formData = new FormData();
+    for (const key in data) {
+        console.log(key, data[key])
+        formData.append(key, data[key]);
+    }
+    xhr.send(formData);
+
+
+    xhr.onload = function () {
+        const responseData = xhr.response;
+        console.log(responseData)
+    };
+
+    xhr.onerror = function() {
+        console.error('Ошибка');
+    };
+}
+
+
+function showAlert(message, type) {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible fade show" role="alert">`,
+            `${message}`,
+            `<button id="alert-close-button" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`,
+        `</div>`
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+
+    const closeButton = document.getElementById("alert-close-button")
+    
+    setTimeout(() => {
+        if (closeButton) {
+            closeButton.click()
+        }
+    }   , 5000);
 }
 
 window.onload = function() {
@@ -302,7 +504,6 @@ window.onload = function() {
         "input",
         filterRoutes
     );
-
 
     guidesTableLanguageSelect.onchange = filterGuides;
     guidesTableWorkExperienceFrom.addEventListener(
@@ -314,7 +515,15 @@ window.onload = function() {
         filterGuides
     );
 
-    
+    orderForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        processFormData();
+    });
+
+    durationModalElement.addEventListener('change', setPrice)
+    personsModalElement.addEventListener('change', setPrice)
+    studentDiscountModalElement.addEventListener('change', setPrice)
+    souvenirModalElement.addEventListener('change', setPrice)
+
     loadRoutes();
-    registerApplication()
 };
